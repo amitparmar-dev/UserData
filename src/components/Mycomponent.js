2aeec0c2354ee2e365c86098a4c66caf496fd5dd
Amit <amitparmar.dev@gmail.com>
2020-08-15 12:22:01 +0530

    commit -m "first commit"

---
+++ src/components/Mycomponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+class MyComponent extends React.Component
+{
+
+
+    render(){
+
+        return(
+        <div>
+            <h1>React App for Users data</h1>
+           
+        </div>);
+    }
+}
+
+export default MyComponent;
